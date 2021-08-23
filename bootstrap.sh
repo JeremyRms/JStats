84a8d465181513a8aac1064dc4f6961f2418d08b
Jeremy <Jeremy@central.tech>
2021-08-23 13:57:21 +0700

    better bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
 
-# creates certificates
-docker-compose -f create-certs.yml run --rm create_certs 
+echo "Tear down previous installation..."
+docker-compose down -v --remove-orphans --timeout 10
 
-# creates and starts containers
+if [ ! -f ".env" ]; then
+    echo "Adding .env file."
+    touch .env
+    {
+        echo '# docker specific settings'
+        echo 'COMPOSE_PROJECT_NAME=JStats'
+        echo 'CERTS_DIR=/usr/share/elasticsearch/config/certificates'
+        echo 'VERSION=7.14.0'
+        echo '# app configuration settings'
+        echo 'NODE_ENV = development'
+        echo 'API_KEY = GITHUBAPIKEY'
+        echo 'HOSTNAME = localhost'
+        echo 'PORT = 3000'
+        echo 'ELASTIC_ENDPOINT = https://es01'
+        echo 'ELASTIC_PORT = 9200'
+        echo 'TIMEZONE = Asia/Bangkok'
+        echo 'ORGANIZATION = GITHUBORGANIZATION'
+        echo 'ELASTIC_PASSWORD = ELASTICUSERPASSWORD'
+} > .env
+
+echo GitHub API Key?
+read -sp 'API KEY: ' API_KEY
+
+sed -i "" "s/GITHUBAPIKEY/$API_KEY/g" .env
+
+echo \nGitHub Organization name?
+read ORGANIZATION
+
+sed -i "" "s/GITHUBORGANIZATION/$ORGANIZATION/g" .env
+fi
+
+echo "Creating certificates..."
+docker-compose -f create-certs.yml run --rm create_certs
+
+echo "Creating and staring containers..."
 docker-compose -f elastic-docker-tls.yml up -d         
 
-# mkdir ~/.elk
+mkdir ~/.elk
 
-# generates root password
+echo "Generating ELK passwords..."
 docker exec es01 /bin/bash -c "bin/elasticsearch-setup-passwords auto --batch --url https://es01:9200"  > ~/.elk/elastic-stack 
 
-# finds and replace the kibana user password in the TLS file
-kibanaPassword=`sed -rn 's/PASSWORD elastic = ([a-zA-Z0-9]*)[:space:]/\1/p' ~/.elk/elastic-stack`
-echo $kibanaPassword
-sed -i "" "s/CHANGEME/$kibanaPassword/g" elastic-docker-tls.yml
+echo "Finds and replace the kibana user password in the .env file..."
+elasticPassword=`sed -rn 's/PASSWORD elastic = ([a-zA-Z0-9]*)/\1/p' ~/.elk/elastic-stack`
+sed -i "" "s/ELASTICUSERPASSWORD/$elasticPassword/g" .env
 
-# restarting...
+echo "Restarting containers..."
 docker-compose stop
 docker-compose -f elastic-docker-tls.yml up -d
 
-# creates certificates
-docker-compose -f run-app.yml run --rm jstats 
+echo "Running the application..."
+docker-compose -f jstats.yml run --rm jstats 
