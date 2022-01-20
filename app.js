6fa54be8d585e7179011acd5de1abe053e917f53
Jeremy <jeremyl@rabbit.co.th>
2022-01-20 18:49:21 +0700

    fix: add optional chaining operator

--- app.js
+++ app.js
@@ -194,58 +194,58 @@ server.listen(port, hostname, () => {
 });
 
 function cleanComment(comment) {
-  delete comment['_links']
-  delete comment['diff_hunk']
-  delete comment['html_url']
-  delete comment['line']
-  delete comment['node_id']
-  delete comment['original_line']
-  delete comment['original_position']
-  delete comment['original_start_line']
-  delete comment['position']
-  delete comment['pull_request_url']
-  delete comment['reactions']
-  delete comment['side']
-  delete comment['start_line']
-  delete comment['start_side']
-  delete comment['user']['gravatar_id'];
-  delete comment['user']['node_id'];
-  delete comment['user']['site_admin'];
-  delete comment['user']['type'];
-  delete comment['user']['url'];
+  delete comment?.['_links']
+  delete comment?.['diff_hunk']
+  delete comment?.['html_url']
+  delete comment?.['line']
+  delete comment?.['node_id']
+  delete comment?.['original_line']
+  delete comment?.['original_position']
+  delete comment?.['original_start_line']
+  delete comment?.['position']
+  delete comment?.['pull_request_url']
+  delete comment?.['reactions']
+  delete comment?.['side']
+  delete comment?.['start_line']
+  delete comment?.['start_side']
+  delete comment?.['user']?.['gravatar_id'];
+  delete comment?.['user']?.['node_id'];
+  delete comment?.['user']?.['site_admin'];
+  delete comment?.['user']?.['type'];
+  delete comment?.['user']?.['url'];
   
-  for (const key in comment['user']) {
+  for (const key in comment?.['user']) {
     if (key.search(/_url/) != -1) {
-      delete comment['user'][key];
+      delete comment?.['user'][key];
     }
   }
 }
 
 function cleanReview(review) {
-  delete review['node_id']
-  delete review['_links']
-  delete review['user']['gravatar_id'];
-  delete review['user']['node_id'];
-  delete review['user']['type'];
+  delete review?.['node_id']
+  delete review?.['_links']
+  delete review?.['user']?.['gravatar_id'];
+  delete review?.['user']?.['node_id'];
+  delete review?.['user']?.['type'];
 
-  for (const key in review['user']) {
+  for (const key in review?.['user']) {
     if (key.search(/_url/) != -1) {
-      delete review['user'][key];
+      delete review?.['user'][key];
     }
   }
 }
 
 function cleanRepo(repository) {
-  delete repository['fork'];
-  delete repository['node_id'];
-  delete repository['size'];
-  delete repository['stargazers_count'];
-  delete repository['watchers'];
-  delete repository['watchers_count'];
+  delete repository?.['fork'];
+  delete repository?.['node_id'];
+  delete repository?.['size'];
+  delete repository?.['stargazers_count'];
+  delete repository?.['watchers'];
+  delete repository?.['watchers_count'];
 
-  delete repository['owner']['gravatar_id']
-  delete repository['owner']['node_id']
-  delete repository['owner']['url']
+  delete repository?.['owner']?.['gravatar_id']
+  delete repository?.['owner']?.['node_id']
+  delete repository?.['owner']?.['url']
 
   for (const key in repository) {
     if (key.search(/_url/) != -1) {
@@ -253,65 +253,65 @@ function cleanRepo(repository) {
     }
   }
 
-  for (const key in repository['owner']) {
+  for (const key in repository?.['owner']) {
     if (key.search(/_url/) != -1) {
-      delete repository['owner'][key];
+      delete repository?.['owner'][key];
     }
   }
 }
 
 function cleanPR(pullRequest) {
-  delete pullRequest['_links'];
-  delete pullRequest['active_lock_reason'];
-  delete pullRequest['diff_url'];
-  delete pullRequest['merge_commit_sha'];
-  delete pullRequest['node_id'];
+  delete pullRequest?.['_links'];
+  delete pullRequest?.['active_lock_reason'];
+  delete pullRequest?.['diff_url'];
+  delete pullRequest?.['merge_commit_sha'];
+  delete pullRequest?.['node_id'];
 
-  delete pullRequest['base']['repo'];
-  delete pullRequest['base']['user'];
+  delete pullRequest?.['base']?.['repo'];
+  delete pullRequest?.['base']?.['user'];
 
-  delete pullRequest['head']['repo']['allow_forking'];
-  delete pullRequest['head']['repo']['archived'];
-  delete pullRequest['head']['repo']['created_at'];
-  delete pullRequest['head']['repo']['disabled'];
-  delete pullRequest['head']['repo']['fork'];
-  delete pullRequest['head']['repo']['forks'];
-  delete pullRequest['head']['repo']['forks_count'];
-  delete pullRequest['head']['repo']['homepage'];
-  delete pullRequest['head']['repo']['pushed_at'];
-  delete pullRequest['head']['repo']['has_issues'];
-  delete pullRequest['head']['repo']['has_projects'];
-  delete pullRequest['head']['repo']['has_downloads'];
-  delete pullRequest['head']['repo']['has_wiki'];
-  delete pullRequest['head']['repo']['has_pages'];
-  delete pullRequest['head']['repo']['is_template'];
-  delete pullRequest['head']['repo']['license'];
-  delete pullRequest['head']['repo']['node_id'];
-  delete pullRequest['head']['repo']['open_issues'];
-  delete pullRequest['head']['repo']['open_issues_count'];
-  delete pullRequest['head']['repo']['owner'];
-  delete pullRequest['head']['repo']['size'];
-  delete pullRequest['head']['repo']['stargazers_count'];
-  delete pullRequest['head']['repo']['topics'];
-  delete pullRequest['head']['repo']['updated_at'];
-  delete pullRequest['head']['repo']['visibility'];
-  delete pullRequest['head']['repo']['watchers'];
-  delete pullRequest['head']['repo']['watchers_count'];
-  delete pullRequest['head']['sha'];
-  delete pullRequest['head']['user'];
+  delete pullRequest?.['head']?.['repo']?.['allow_forking'];
+  delete pullRequest?.['head']?.['repo']?.['archived'];
+  delete pullRequest?.['head']?.['repo']?.['created_at'];
+  delete pullRequest?.['head']?.['repo']?.['disabled'];
+  delete pullRequest?.['head']?.['repo']?.['fork'];
+  delete pullRequest?.['head']?.['repo']?.['forks'];
+  delete pullRequest?.['head']?.['repo']?.['forks_count'];
+  delete pullRequest?.['head']?.['repo']?.['homepage'];
+  delete pullRequest?.['head']?.['repo']?.['pushed_at'];
+  delete pullRequest?.['head']?.['repo']?.['has_issues'];
+  delete pullRequest?.['head']?.['repo']?.['has_projects'];
+  delete pullRequest?.['head']?.['repo']?.['has_downloads'];
+  delete pullRequest?.['head']?.['repo']?.['has_wiki'];
+  delete pullRequest?.['head']?.['repo']?.['has_pages'];
+  delete pullRequest?.['head']?.['repo']?.['is_template'];
+  delete pullRequest?.['head']?.['repo']?.['license'];
+  delete pullRequest?.['head']?.['repo']?.['node_id'];
+  delete pullRequest?.['head']?.['repo']?.['open_issues'];
+  delete pullRequest?.['head']?.['repo']?.['open_issues_count'];
+  delete pullRequest?.['head']?.['repo']?.['owner'];
+  delete pullRequest?.['head']?.['repo']?.['size'];
+  delete pullRequest?.['head']?.['repo']?.['stargazers_count'];
+  delete pullRequest?.['head']?.['repo']?.['topics'];
+  delete pullRequest?.['head']?.['repo']?.['updated_at'];
+  delete pullRequest?.['head']?.['repo']?.['visibility'];
+  delete pullRequest?.['head']?.['repo']?.['watchers'];
+  delete pullRequest?.['head']?.['repo']?.['watchers_count'];
+  delete pullRequest?.['head']?.['sha'];
+  delete pullRequest?.['head']?.['user'];
   
-  delete pullRequest['user']['gravatar_id'];
-  delete pullRequest['user']['node_id'];
-  delete pullRequest['user']['type'];
+  delete pullRequest?.['user']?.['gravatar_id'];
+  delete pullRequest?.['user']?.['node_id'];
+  delete pullRequest?.['user']?.['type'];
 
-  delete pullRequest['assignee']['gravatar_id'];
-  delete pullRequest['assignee']['node_id'];
-  delete pullRequest['assignee']['type'];
+  delete pullRequest?.['assignee']?.['gravatar_id'];
+  delete pullRequest?.['assignee']?.['node_id'];
+  delete pullRequest?.['assignee']?.['type'];
   
 
-  for (const key in pullRequest['user']) {
+  for (const key in pullRequest?.['user']) {
     if (key.search(/_url/) != -1) {
-      delete pullRequest['user'][key];
+      delete pullRequest?.['user'][key];
     }
   }
 
@@ -321,27 +321,27 @@ function cleanPR(pullRequest) {
     }
   }
 
-  for (const key in pullRequest['assignee']) {
+  for (const key in pullRequest?.['assignee']) {
     if (key.search(/_url/) != -1) {
-      delete pullRequest['assignee'][key];
+      delete pullRequest?.['assignee'][key];
     }
   }
 
-  for (const assignee in pullRequest['assignees']) {
-    for (const key in pullRequest['assignees'][assignee]) {
+  for (const assignee in pullRequest?.['assignees']) {
+    for (const key in pullRequest?.['assignees'][assignee]) {
       if (key.search(/_url/) != -1) {
-        delete pullRequest['assignees'][assignee][key];
+        delete pullRequest?.['assignees'][assignee][key];
       }
 
-      delete pullRequest['assignees'][assignee]['gravatar_id'];
-      delete pullRequest['assignees'][assignee]['node_id'];
-      delete pullRequest['assignees'][assignee]['type'];
+      delete pullRequest?.['assignees'][assignee]?.['gravatar_id'];
+      delete pullRequest?.['assignees'][assignee]?.['node_id'];
+      delete pullRequest?.['assignees'][assignee]?.['type'];
     }
   }
 
-  for (const key in pullRequest['head']['repo']) {
+  for (const key in pullRequest?.['head']?.['repo']) {
     if (key.search(/_url/) != -1) {
-      delete pullRequest['head']['repo'][key];
+      delete pullRequest?.['head']?.['repo'][key];
     }
   }
 }
